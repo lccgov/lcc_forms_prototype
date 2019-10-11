a79a780e6bc335ccb23e3153125c69d0e2455aee
Ben Hodge <benjamin.hodge@leeds.gov.uk>
2019-10-11 14:45:38 +0100

    form example of REMS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,34 @@
 //input block label
-$('input:radio').click(function() {
-    $('input:radio[name='+$(this).attr('name')+']').parent().removeClass('active');
-        $(this).parent().addClass('active');
-});
-
-//input block label
 $('input:checkbox').click(function() {
     	$(this).parent().toggleClass('active');
 });
+
+//input block label
+if($('.form-group--radio').length > 0) {
+    $('.form-group--radio > input').on('click', function() {
+        $('.form-group--radio').removeClass('display');
+        $(this).parent().addClass('display');
+    });
+
+    $('.option--sub > input').on('click', function() {
+        $('.option--sub').removeClass('display');
+        $(this).parent().addClass('display');
+    });
+
+} else {
+    $('input:radio').click(function() {
+        $('input:radio[name='+$(this).attr('name')+']').parent().removeClass('active');
+            $(this).parent().addClass('active');
+    });
+}
+
+// Select - other
+if( $('.form-control').length > 0 ) {
+    $('select').on('change', function() {
+        $(this).parent().removeClass('active');
+        var value = this.value;
+        if( value == 'Other' ) {
+            $(this).parent().addClass('active');
+        }
+    });
+}
